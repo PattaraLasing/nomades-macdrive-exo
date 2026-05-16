1d38a289e4e60fb8007d79896cf5b1c18caaf091
PattaraLasing <pattara.lasing@proton.me>
2026-05-16 15:38:25 +0200

    complete succes and checkout page

--- src/app/pages/success-page/success-page.ts
+++ src/app/pages/success-page/success-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import JSConfetti from 'js-confetti'
 
 @Component({
   selector: 'app-success-page',
@@ -6,4 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './success-page.html',
   styleUrl: './success-page.css',
 })
-export class SuccessPage {}
+export class SuccessPage implements OnInit {
+
+  private readonly _route = inject(ActivatedRoute);
+  
+  public readonly orderId = signal<string | undefined>(undefined);
+
+  ngOnInit(): void {
+    const orderId = this._route.snapshot.queryParams['id'];
+    this.orderId.set(orderId);
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }
+
+}
